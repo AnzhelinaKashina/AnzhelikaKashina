193a056edbc0667f97698ec09205e5676f4c9ff0
Anzhelikaa <113301109+AnzhelinaKashina@users.noreply.github.com>
2023-06-05 23:42:37 +0300

    было тяжело....

---
+++ 4кт/42/interv_4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class interv_4 {
+    public static void main(String[] args) {
+        Scanner vvod = new Scanner(System.in);
+        System.out.print("Введите количество интервалов: ");
+        int n = vvod.nextInt();
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.println("Введите левую и правую границы интервала " + (i + 1));
+            int left = vvod.nextInt();
+            int right = vvod.nextInt();
+            count += right - left ;
+        }
+        System.out.println("Количество чисел между заданными интервалами: " + count);
+    }
+}
---
+++ 4кт/44/str_6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class str_6 {
+    public static void main(String[] args) {
+        Scanner vvod = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        String first = vvod.nextLine();
+        System.out.print("Введите второе число: ");
+        String second = vvod.nextLine();
+        int firstNum = Integer.parseInt(first);
+        int secondNum = Integer.parseInt(second);
+        int sum = firstNum + secondNum;
+        System.out.println("Результат: " + Integer.toString(sum));
+        }
+    }
+
